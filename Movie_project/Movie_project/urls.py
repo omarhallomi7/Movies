4b549d27a0bb08d910fa54b9557ed435c341b91d
Omar <omar@example.com>
2024-09-15 21:18:14 +0300

    login and sigup

--- Movie_project/Movie_project/urls.py
+++ Movie_project/Movie_project/urls.py
@@ -3,11 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from movies import views as movie_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
     path('accounts/', include('accounts.urls')),
+    path('', movie_views.movie_list, name="home")
 ]
 
 if settings.DEBUG:
